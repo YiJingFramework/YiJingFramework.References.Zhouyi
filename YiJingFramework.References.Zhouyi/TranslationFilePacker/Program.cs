93769d435050a63f6dcca35e431c28867c1da968
yueyinqiu <lujiaqi2003@126.com>
2021-03-18 19:44:46 +0800

    add packer

---
+++ YiJingFramework.References.Zhouyi/TranslationFilePacker/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
+
+namespace TranslationFilePacker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Translation File Packer For YiJingFramework.References.Zhouyi.");
+                Console.WriteLine("Package version: 2.0.0.");
+                Console.WriteLine("Format: <InputFile> <OutputFile>");
+                return;
+            }
+
+            var inp = File.ReadAllText(args[0]);
+            JsonSerializerOptions options
+               = new JsonSerializerOptions() {
+                   AllowTrailingCommas = true,
+                   ReadCommentHandling = JsonCommentHandling.Skip,
+                   WriteIndented = false,
+                   Encoder = JavaScriptEncoder.Create(UnicodeRanges.All)
+               };
+            var translation = JsonSerializer.Deserialize<TranslationFile>(
+                     inp, options);
+            File.WriteAllText(args[1], JsonSerializer.Serialize(translation, options));
+        }
+    }
+}
---
+++ YiJingFramework.References.Zhouyi/TranslationFilePacker/TranslationFile.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static TranslationFilePacker.TranslationFile;
+using static TranslationFilePacker.TranslationFile.TranslationFileHexagrams;
+
+namespace TranslationFilePacker
+{
+    internal sealed record TranslationFile(
+        TranslationFileTrigrams Trigrams,
+        TranslationFileNumbers Numbers,
+        TranslationFilePatterns Patterns,
+        TranslationFileHexagrams Hexagrams)
+    {
+        internal bool Check()
+        {
+            return this.Trigrams.Check() &&
+                this.Numbers.Check() &&
+                this.Patterns.Check() &&
+                this.Hexagrams.Check();
+        }
+        private static bool CheckLongerAndNullValue<T>(T[] array, int length) where T : class
+        {
+            return array is not null &&
+                array.Length >= length &&
+                !array.Contains(null);
+        }
+        private static bool CheckLengthAndNullValue<T>(T[] array, int length) where T : class
+        {
+            return array is not null &&
+                array.Length == length &&
+                !array.Contains(null);
+        }
+        internal sealed record TranslationFileTrigrams(string[] Names, string[] Natures)
+        {
+            internal bool Check()
+            {
+                return CheckLengthAndNullValue(this.Names, 8) && CheckLengthAndNullValue(this.Natures, 8);
+            }
+        }
+        internal sealed record TranslationFileNumbers(string[] Ordinal)
+        {
+            internal bool Check()
+            {
+                return CheckLongerAndNullValue(this.Ordinal, 64);
+            }
+        }
+        internal sealed record TranslationFilePatterns(
+            string[] YinLines,
+            string[] YangLines,
+            string HexagramsToString,
+            string PureHexagramsToString,
+            string ApplyNines,
+            string ApplySixes)
+        {
+            internal bool Check()
+            {
+                return this.HexagramsToString is not null &&
+                    this.PureHexagramsToString is not null &&
+                    this.ApplyNines is not null &&
+                    this.ApplySixes is not null &&
+                    CheckLengthAndNullValue(this.YinLines, 6) && CheckLengthAndNullValue(this.YangLines, 6);
+            }
+        }
+        internal sealed record TranslationFileHexagrams(
+            TranslationFileHexagramNameAndTexts[] NamesAndTexts,
+            string ApplyNines,
+            string ApplySixes)
+        {
+            internal sealed record TranslationFileHexagramNameAndTexts(
+                string[] Lines,
+                string Name,
+                string Text)
+            {
+                internal bool Check()
+                {
+                    return this.Name is not null &&
+                        this.Text is not null &&
+                        CheckLengthAndNullValue(this.Lines, 6);
+                }
+            }
+            internal bool Check()
+            {
+                if (this.ApplyNines is not null &&
+                    this.ApplySixes is not null &&
+                    CheckLengthAndNullValue(this.NamesAndTexts, 64))
+                {
+                    foreach (var nat in this.NamesAndTexts)
+                    {
+                        if (!nat.Check())
+                            return false;
+                    }
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
